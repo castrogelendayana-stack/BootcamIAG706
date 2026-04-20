dc8a635820d9d757d19185a7b713ee8e41fea77d
Gelen <castrogelendayana@gmail.com>
2026-04-20 11:38:07 -0500

    act202

---
+++ mision2/chatv1/chatv1.py
@@ -0,0 +1,5 @@
+from sklearn.feature_extraction.text import CountVectorizer
+# CountVectorizer convierte texto en un vector
+from  sklearn.naive_bayes import MultinomialNB
+"""
+MultinomialNB  modelo de inteligencia artificial que aprenden relaciones entre texto y respuesta."""
